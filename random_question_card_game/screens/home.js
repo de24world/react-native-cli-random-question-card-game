f3dbfab7cf5a73f2b6b6b10443ab2c0cad2eee94
HoSoo Lee <hosoo.lee@arithnea.de>
2021-01-10 02:17:35 +0100

    inital files & global styles files created

---
+++ random_question_card_game/screens/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {globalStyles} from '../styles/global';
+
+const Home = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text>home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
---
+++ random_question_card_game/styles/global.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flext: 1,
+    padding: 20,
+  },
+  titleText: {
+    // fontFamily: 'sample'
+    fontSize: 18,
+  },
+});
