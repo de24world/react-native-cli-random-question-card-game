35a714a09dfa73ab690187a1e16662685aa6a55d
HoSoo Lee <hosoo.lee@arithnea.de>
2021-01-12 00:40:23 +0100

    typescript first step

---
+++ random_question_card_game/data/Interfaces.tsx
@@ -0,0 +1,8 @@
+export interface CardJsons {
+  data: {
+    questions: {
+      id: number;
+      text: string;
+    };
+  };
+}
--- random_question_card_game/screens/card.tsx
+++ random_question_card_game/screens/card.tsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import data from '../data/question(en).json';
+import CardJsons from '../data/Interfaces';
 
-export default function Card() {
-  const [questions, setQuestions] = useState('');
+const Card: React.FC<CardJsons> = () => {
+  const [questions, setQuestions] = useState(data);
 
   const getQuestion = () => {
     let randomNum = Math.floor(Math.random() * data.length);
@@ -15,76 +16,16 @@ export default function Card() {
   }, []);
 
   return (
-    <View style={styles.Cotainer}>
-      <View style={styles.TheCard}>
-        <View styles={styles.TheBack}>{/* HearOutlined? */}</View>
-
-        <View styles={styles.TheFront}>
-          <Text>{questions.id}/100</Text>
-          <Text>{questions.text}?</Text>
-          <Button title="Next" color="red" onPress={getQuestion} />
-        </View>
+    <>
+      <View>
+        <Text>{questions.id}/100</Text>
+        <Text>{questions.text}?</Text>
+        <Button title="Next" color="red" onPress={getQuestion} />
       </View>
-    </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  /*
-  const Container = styled.div`
-  width: 250px;
-  height: 320px;
-  display: block;
-  margin: 0px auto;
-`;
+const styles = StyleSheet.create({});
 
-const TheCard = styled.div`
-  width: 100%;
-  height: 100%;
-  transform-style: preserve-3d;
-  transition: all 0.8s ease;
-  &:hover {
-    transform: rotateY(180deg);
-  }
-`;
-
-const TheBack = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: ${(props) => props.theme.colors.danger};
-  border-radius: 10px;
-`;
-
-const TheFront = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-width: 5px;
-  border-radius: 10px;
-  border-style: double;
-  border-color: black;
-  backface-visibility: hidden;
-  overflow: hidden;
-  background: white;
-  text-align: center;
-  transform: rotateY(180deg);
-`;
-
-const Text = styled.h3`
-  vertical-align: middle;
-  text-align: center;
-  border-radius: 10px;
-  color: white;
-  padding: 10px;
-  margin: 15px;
-  height: 180px;
-  background-color: ${(props) => props.theme.colors.danger};
-`;
-
-const StyledButton = styled(Button)`
-  position: absolute;
-`;
-*/
-});
+export default Card;
